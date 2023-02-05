ba6d4f9330864d20572093ca5a825688fdb4e0c6
Joey Tsui <joeytsui131@gmail.com>
2023-02-05 12:35:06 -0500

    backend auth finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,41 @@
 class ApplicationController < ActionController::API
+    include ActionController::RequestForgeryProtection
+
+    rescue_from StandardError, with: :unhandled_error
+    rescue_from ActionController::InvalidAuthenticityToken,
+        with: :invalid_authenticity_token
+        
+    protect_from_forgery with: :exception
+    before_action :snake_case_params
+    before_action :attach_authenticity_token
+
+    def current_user
+        @current_user ||= User.find_by(session_token: session[:session_token])
+    end
+
+    def login!(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def logout!
+        current_user.reset_session_token! if current_user
+        session[:session_token] = nil
+        @current_user = nil
+    end
+
+    def require_logged_in
+        unless current_user
+            render json: { message: "Unauthorized" }, status: :unauthorized
+        end
+    end
+    private
+
+    def snake_case_params
+    params.deep_transform_keys!(&:underscore)
+    end
+
+    def attach_authenticity_token
+    headers["X-CSRF-Token"] = masked_authenticity_token(session)
+    # headers['X-CSRF-Token'] = form_authenticity_token
+    end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+    has_secure_password
+    validates :email, :password_digest, :session_token, presence: true
+    validates :email, :session_token, uniqueness: true
+    validates :email, length: { in: 3..255 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    before_validation :ensure_session_token
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+        # has_secure_password gives us the authenticate method
+        if user&.authenticate(password)
+            return user
+        else
+            nil
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        save!
+        session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        while true
+            token = SecureRandom.urlsafe_base64
+            return token unless User.exists?(session_token: token)
+        end
+    end
+end
